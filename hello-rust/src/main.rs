a25b4939cda06e51ea385ecae9609b8bd5becf3b
Liang Zheng <zhengliang0901@gmail.com>
2022-01-06 10:52:57 +0000

    add examples of chapter 5,6
    Signed-off-by: Liang Zheng <zhengliang0901@gmail.com>

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,7 +1,30 @@
+#![allow(unused)]
 extern crate ferris_says;
 
 use ferris_says::say;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::fs::File;
 use std::io::{stdout, BufWriter};
+
+struct HasDrop;
+
+impl Drop for HasDrop {
+    fn drop(&mut self) {
+        println!("Dropping HasDrop!");
+    }
+}
+
+struct HasTwoDrops {
+    one: HasDrop,
+    two: HasDrop,
+}
+
+impl Drop for HasTwoDrops {
+    fn drop(&mut self) {
+        println!("Dropping HasTwoDrops!");
+    }
+}
 
 fn main() {
     println!("===============================================================");
@@ -70,6 +93,9 @@ fn main() {
     for element in a.iter() {
         println!("the value is: {}", element);
     }
+    for (i, element) in a.iter().enumerate() {
+        println!("the value of {} is: {}", i, element);
+    }
 
     println!("===============================================================");
     for number in (1..4).rev() {
@@ -79,13 +105,349 @@ fn main() {
     println!("LIFTOFF!!!");
 
     println!("===============================================================");
+    #[derive(Copy, Clone)]
+    struct Foo(u8);
+
+    let x = 1;
+    let y = Foo(2);
+    drop(x); // a copy of `x` is moved and dropped
+    drop(y); // a copy of `y` is moved and dropped
+
+    println!("x: {}, y: {}", x, y.0); // still available
+
+    println!("===============================================================");
+    // https://doc.rust-lang.org/std/mem/fn.drop.html
+    let x = RefCell::new(1);
+
+    let mut mutable_borrow = x.borrow_mut();
+    *mutable_borrow = 1;
+
+    drop(mutable_borrow); // relinquish the mutable borrow on this slot
+
+    let borrow = x.borrow();
+    println!("{}", *borrow);
+
+    println!("===============================================================");
+    // https://doc.rust-lang.org/std/ops/trait.Drop.html#tymethod.drop
+    {
+        let _x = HasTwoDrops {
+            one: HasDrop,
+            two: HasDrop,
+        };
+        println!("Running!");
+    }
+
+    println!("===============================================================");
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    // let s3 = s1; // borrow of moved value: `s1`
+
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
+
+    println!("===============================================================");
+    let mut s = String::from("hello");
+
+    let r1 = &s; // 没问题
+    let r2 = &s; // 没问题
+    println!("{} and {}", r1, r2);
+    // 此位置之后 r1 和 r2 不再使用
+
+    let r3 = &mut s; // 没问题
+    println!("{}", r3);
+
+    println!("===============================================================");
+    let s = String::from("hello world");
+    let word = first_word(&s);
+    // s.clear(); // cannot borrow `s` as mutable because it is also borrowed as immutable
+    println!("the first world is: {}", word);
+
+    let word = first_word(&s[..]);
+    println!("the first world is: {}", word);
+
+    let s = "hi world"; // s 的类型是 &str：它是一个指向二进制程序特定位置的 slice
+    let word = first_word(s);
+    println!("the first world is: {}", word);
+
+    println!("===============================================================");
+    let user1 = User {
+        username: String::from("hi"),
+        email: String::from("xxx@gmail.com"),
+        active: true,
+        sign_in_count: 2,
+    };
+    let user2 = User {
+        username: String::from("hello"),
+        email: String::from("yyy@gmail.com"),
+        ..user1 // struct update syntax
+    };
+
+    #[derive(Debug)]
+    struct Color(i32, i32, i32);
+    #[derive(Debug)]
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    dbg!(&user1);
+    println!("{:#?}, {:#?}, {:#?}, {:#?}", user1, user2, black, origin);
+    // println!("{:?}, {:?}, {:?}, {:?}", user1, user2, black, origin);
+
+    println!("===============================================================");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("{:#?}", Rectangle::square(20));
+
+    println!("===============================================================");
+    let some_number = Some(5);
+    let some_string = Some("string");
+    let absent_number: Option<i32> = None;
+    println!("absent_number.is_none(): {}", absent_number.is_none());
+    println!("some_number.is_none(): {}", some_number.is_none());
+    println!("some_number.is_some(): {}", some_number.is_some());
+
+    let text: Option<String> = Some("Hello, world!".to_string());
+    // First, cast `Option<String>` to `Option<&String>` with `as_ref`,
+    // then consume *that* with `map`, leaving `text` on the stack.
+    let text_length: Option<usize> = text.as_ref().map(|s| s.len());
+    println!("still can print text: {:?}", text);
+
+    let mut x = Some(2);
+    match x.as_mut() {
+        Some(v) => *v = 42,
+        None => {}
+    }
+    println!("mut x: {:?}", x);
+
+    println!("=======================match========================================");
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    fn move_player(num_spaces: u8) {}
+
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    } else {
+        println!("Others");
+    }
+
+    println!("=============================vector==================================");
+    let mut v = vec![1, 2, 3];
+    v.push(4);
+    println!("vector: {:?}", v);
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+    let second: i32 = v[1];
+    println!("The second element is {}", second);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+    v.pop();
+    println!("vector: {:?}", v);
+    for i in &v {
+        println!("{}", i);
+    }
+
+    // let mut v = vec![1, 2, 3, 4, 5];
+    // let first = &v[0];
+    // v.push(6); // cannot borrow `v` as mutable because it is also borrowed as immutable
+    // println!("The first element is: {}", first);
+
+    println!("=============================String==================================");
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("after push_str, s1 is {}", s1);
+    println!("s2 is {}", s2);
+    s1.push('x');
+    println!("after push, s1 is {}", s1);
+    println!("===============================================================");
+    let mut s = String::from("hello");
+    s.push_str(", world");
+    println!("s = {}", s);
+
+    println!("===============================================================");
+    let s1 = String::from("hello");
+    // let s2 = s1;
+    // println!("s1 = {}", s1); // error[E0382]: borrow of moved value: `s1`
+    //
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    println!("===============================================================");
+    let x = 5;
+    let y = x;
+    println!("x = {}, y = {}", x, y);
+
+    println!("===============================================================");
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // 注意 s1 被移动了，不能继续使用
+    println!("s3 = {}", s3);
+    let s4 = s3 + " haha";
+    println!("s4 = {}", s4);
+
+    println!("===============================================================");
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("s = {}", s);
+
+    println!("===============================================================");
+    let hello = "Здравствуйте";
+    let s = &hello[0..4];
+    println!("s = {}", s);
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
+
+    println!("=============================HashMap==================================");
+    let mut scores = HashMap::new();
+    scores.insert(String::from("hello"), 1);
+    scores.insert(String::from("hi"), 2);
+    println!("scores = {:?}", scores);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("scores = {:?}", scores);
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+    println!("score = {:?}", score);
+    match score {
+        Some(n) => println!("score = {}", n),
+        None => println!("team not exists"),
+    };
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    println!("===============================================================");
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    let e = scores.entry(String::from("Yellow"));
+    println!("entry = {:#?}", e);
+    e.or_insert(50);
+    let e = scores.entry(String::from("Blue"));
+    println!("entry = {:#?}", e);
+    e.or_insert(50);
+    println!("{:?}", scores);
+
+    println!("===============================================================");
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        // or_insert 方法在键对应的值存在时就返回这个值的可变引用，如果不存在则将参数作为新值插入并返回新值的可变引用。
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("map = {:?}", map);
+
+    println!("=============================error==================================");
+    let f = File::open("hello.txt");
+    println!("f = {:?}", f);
+
     println!("===============================================================");
     println!("===============================================================");
-    println!("===============================================================");
-    println!("===============================================================");
+    println!("=============================panic==================================");
+    let v = vec![1, 2, 3];
+    v[99];
+}
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    // associated function
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
 }
 
 fn plus_one(x: i32) -> i32 {
     // return x + 1;
     x + 1
+}
+
+// fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
