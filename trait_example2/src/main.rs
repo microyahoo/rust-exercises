f3c15afd78d0c32c77e2fdf416599fdf0298bbd5
Liang Zheng <zhengliang0901@gmail.com>
2022-02-08 23:28:07 +0800

    trait object
    Signed-off-by: Liang Zheng <zhengliang0901@gmail.com>

--- trait_example2/src/main.rs
+++ trait_example2/src/main.rs
@@ -1,5 +1,7 @@
 use std::io::Write;
 
+// ================================================================================================
+// https://cotigao.medium.com/dyn-impl-and-trait-objects-rust-fd7280521bea
 trait Animal {
     fn talk(&self);
 }
@@ -65,6 +67,52 @@ fn animal() -> Box<dyn Animal> {
 //     Cat {}
 // }
 
+// ================================================================================================
+// https://stackoverflow.com/questions/27567849/what-makes-something-a-trait-object
+struct Point {
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+trait Print {
+    fn print(&self);
+}
+
+// dyn Print is actually a type and we can implement methods on it
+impl dyn Print + 'static {
+    fn print_traitobject(&self) {
+        println!("from trait object");
+    }
+}
+
+impl Print for Point {
+    fn print(&self) {
+        println!("x: {}, y: {}, z: {}", self.x, self.y, self.z);
+    }
+}
+
+// static dispatch (compile time): compiler must know specific versions
+// at compile time generates a version for each type
+
+// compiler will use monomorphization to create different versions of the function
+// for each type. However, because they can be inlined, it generally has a faster runtime
+// compared to dynamic dispatch
+fn static_dispatch<T: Print>(point: &T) {
+    point.print();
+}
+
+// dynamic dispatch (run time): compiler doesn't need to know specific versions
+// at compile time because it will use a pointer to the data and the vtable.
+// The vtable contains pointers to all the different different function implementations.
+// Because it has to do lookups at runtime it is generally slower compared to static dispatch
+
+// point_trait_obj is a trait object
+fn dynamic_dispatch(point_trait_obj: &(dyn Print + 'static)) {
+    point_trait_obj.print();
+    point_trait_obj.print_traitobject();
+}
+
 fn main() {
     let d = Dog {};
     let c = Cat {};
@@ -74,6 +122,15 @@ fn main() {
     let mut buf: Vec<u8> = vec![];
     // let writer: Write = buf;
     let writer: &mut dyn Write = &mut buf;
-}
 
-// https://cotigao.medium.com/dyn-impl-and-trait-objects-rust-fd7280521bea
+    println!("================================================================");
+
+    let point = Point { x: 1, y: 2, z: 3 };
+
+    // On the next line the compiler knows that the generic type T is Point
+    static_dispatch(&point);
+
+    // This function takes any obj which implements Print trait
+    // We could, at runtime, change the specfic type as long as it implements the Print trait
+    dynamic_dispatch(&point);
+}
